c90862dc146fc7429149fe152824dc9fb2b52b34
Liam Naylor <liam@liamnaylor.com>
2022-10-13 15:35:58 -0700

    removing extra line breaks

--- spinner2.js
+++ spinner2.js
@@ -1,13 +1,11 @@
 const positions = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   '];
 let time = 0;
-
 for (const position of positions) {
   time += 150;
   setTimeout(() => {
     process.stdout.write(position);
   }, time);
 };
-
 setTimeout(() => {
   process.stdout.write('\n');
 }, time);
